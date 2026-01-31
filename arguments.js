c43a819fa034e6bc02a77d20f60ff7caed48728f
Md-Rifat-Pk <mdrifat669900@gmail.com>
2026-01-31 23:43:15 +0600

    first commit

---
+++ arguments.js
@@ -0,0 +1,47 @@
+// function macherTorkari(mach, shobji, mosla) {
+//     // রান্নার নির্দেশাবলী (কনসোল লগ)
+//     console.log("তেল গরম করুন");
+//     console.log("মসলা ফোঁড়ন");
+//     console.log("শাকসবজি দিন");
+//     console.log(mach + " দিন");
+
+//     // রান্নার শেষ (রিটার্ন ভ্যালু)
+//     return "সুস্বাদু মাছের তরকারি প্রস্তুত!";
+// }
+
+// // ফাংশন কল
+// const result = macherTorkari("ইলিশ", "আলু", "হলুদ");
+// console.log(result); // ফলাফল প্রিন্ট করুন
+
+
+
+
+
+
+
+
+// function doubleIt (number){
+//     const doubled = number * 2;
+//     console.log(number, doubled);
+// }
+
+// console.log('I well call the function')
+// doubleIt(15);
+// console.log('----------');
+// doubleIt(88);
+// console.log(' ----------');
+// doubleIt(873)
+// const number = 55;
+// doubleIt(number);
+// const money = 112;
+// doubleIt(112);
+
+
+function difference( num1, num2){
+    const diff = num1 - num2;
+    console.log(num1, num2, 'difference is', diff);
+}
+
+const fatherAge = 40;
+const myAge = 10;
+difference(fatherAge, myAge);
---
+++ parameter.js
@@ -0,0 +1,38 @@
+function vatKhao() {
+    console.log('hand wash kore acho');
+    console.log('boso');
+    console.log('plate e khabar dhabar nao');
+    console.log('khawa suru koro');
+}
+
+// vatKhao();
+
+// 3 --> 3*3 = 9
+// 4 --> 4*4 = 16
+// 5 --> 5*5 = 25
+// 11 --> 11*11 = 121
+ 
+// function square(number){
+// console.log('value of the number parameter', number);
+// const borgo = number * number;
+// console.log('square of the given number is: ', borgo);
+// }
+// square(110);
+// console.log( '--------');
+// square(10);
+// square(15);
+
+// function add(num1, num2){
+//     const sum = num1 + num2;
+//     console.log(sum);
+// }
+
+// add(5, 71);
+// add(60, 60);
+
+function addAll(a, b, c, d, e){
+    const total = a + b + c + d + e;
+    console.log(total);
+}
+
+addAll(3, 4, 5, 6, 7);
